3f21c9b7759c297321003b4a45088ba2e57ae3ae
Eunhyeong Bae <32235719+E2H2@users.noreply.github.com>
2020-09-14 23:42:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,17 @@ fig8 = gcs.makeFigRel()
 
 fig9 = oecdfig.makeFigure()
 
+fig10 = gldp.makeFigure()
+
+
 app.layout = html.Div([
     html.H1('의료인력 데이터 분석'),
+
+    html.H3('▶우리나라 의사 인력 분석'),
+    html.H6('≫ 연도별 의료인력 증감 추이'),
+    dcc.Graph(id='graph7', figure=fig7),
+
+    html.H3('▶지역별 의사 인력 분석'),
 
     html.H6('≫ 연도별 지역별 의사수 추이'),
     dcc.Graph(id='graph6', figure=fig6),
@@ -42,15 +51,26 @@ app.layout = html.Div([
     html.H6('≫ 연도별 지역별 의사수'),
     dcc.Graph(id='graph5', figure=fig5),
 
+    html.H6('≫ 우리나라 지역별 1000명당 의사수 + OECD회원국 1000명당 의사수'),
+    dcc.Graph(id='graph10', figure=fig10),
+
+    html.H3('▶지역별 의사 전공과목 분석'),
+
     html.H6('≫ 연도별 지역, 과별 의사수'),
     dcc.Graph(id='graph4', figure=fig4),
 
     html.H6('≫ 연도별 지역, 과별 의사수, 상대적 수치'),
     dcc.Graph(id='graph8', figure=fig8),
 
-    html.H6('≫ 연도별 의료인력 증감 추이'),
-    dcc.Graph(id='graph7', figure=fig7),
 
+    # html.H6('≫ 우리나라 지역별 1000명당 의사수'),
+    # dcc.Graph(id='graph8', figure=fig8),
+
+    # html.H6('≫ 우리나라 지역별 1000명당 의사수 + OECD회원국 1000명당 의사수'),
+    # dcc.Graph(id='graph10', figure=fig10),
+
+
+    html.H3('▶OECD 의사 증가율 회귀 분석'),
     html.H6('≫ OECD 국가별 의사 수와 추이 예측'),
     dcc.Graph(id='graph9', figure=fig9)
 ])
@@ -58,4 +78,3 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug=False)
-
