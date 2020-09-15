ebd763721b482308590b1808ae326d2629a13dd0
unsik-kim <loudic92@gmail.com>
2020-09-15 09:47:09 +0900

    commit

--- graphpkg/LocalDocPerPop.py
+++ graphpkg/LocalDocPerPop.py
@@ -2,7 +2,6 @@ import pymongo
 from pymongo import MongoClient
 import pandas as pd
 import numpy as np
-import seaborn as sns
 from scipy import stats
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
